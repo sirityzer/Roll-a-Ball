f654496e12f05f2108361978f1b013f0f538277b
sirityzer <sirityzer@gmail.com>
2019-01-26 14:06:52 -0500

    added triggers and count/win text

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
+using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
+    private int count;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetcountText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -21,5 +28,23 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+       if(other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetcountText();
+        }
+    }
+
+    void SetcountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
     }
 }
